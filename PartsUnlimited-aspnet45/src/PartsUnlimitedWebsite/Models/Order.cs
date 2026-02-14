5d3da03ddb58a0fd3d9ee988d8aa45186e994168
beatazalewa <info@zalnet.pl>
2026-02-14 11:45:38 +0100

    PTX:3333: Added second method

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Order.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Order.cs
@@ -66,5 +66,6 @@ namespace PartsUnlimited.Models
         public List<OrderDetail> OrderDetails { get; set; }
 
         /* A code of very smart method is here */
+        /* A code of very smart second method is here */
     }
 }
